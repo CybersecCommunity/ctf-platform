9aca7d9a21e23c9303e9986269e92c4a03d11409
Neha Kantheti <nehakantheti@gmail.com>
2025-07-06 23:20:27 +0530

    Added deployed link to the pin challenge and added robots challenge

---
+++ app/somethings-hidden-here/page.tsx
@@ -0,0 +1,12 @@
+export default function HiddenFlagPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <div className="text-center">
+        <h1 className="text-2xl font-bold">🎉 Flag Found!</h1>
+        <p className="mt-4">
+          {"CTF{have_you_checked_about_webcrawlers}"}
+        </p>
+      </div>
+    </div>
+  );
+}
